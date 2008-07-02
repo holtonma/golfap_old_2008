3c899a5abfcc5eddd15b13a46eed4b92fd2e1fe9
Mark Holton <holtonma@Macintosh-69.local>
2008-07-02 16:45:00 -0700

    separation of concerns for Rounds... breaking out methods into Model...

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -14,8 +14,8 @@ class RoundsController < ApplicationController
     #sometimes      : /players/:player_id/rounds?m=6&y=2008
     @month = params[:m] || Time.now
     @year = params[:y] || Time.now
-    @rounds_per_month = rounds_per_month_desc
-    @rounds_hash = db_rounds_per_month_year
+    @rounds_per_month = Round.rounds_per_month_desc
+    @rounds_hash = Round.db_rounds_per_month_year
   end
 
   def show
@@ -37,46 +37,4 @@ class RoundsController < ApplicationController
   def destroy
   end
   
-  private
-    def db_rounds_per_month_year
-      # num_rounds month_num year_num
-      rounds_by_month_year = Round.find(:all, 
-        :select => "Count(*) as num_rounds, EXTRACT(MONTH FROM date_played) as month_num,  EXTRACT(YEAR FROM date_played) as year_num", 
-        :group => "EXTRACT(YEAR FROM date_played), EXTRACT(MONTH FROM date_played)",
-        :order => "year_num DESC, month_num DESC",
-        :limit => 12)
-      
-      rounds_by_month_year
-    end
-    
-    def rounds_per_month_desc
-      # 1.months.ago
-      # select Count(*), EXTRACT(MONTH FROM date_played),  EXTRACT(YEAR FROM date_played) 
-      # from rounds GROUP BY EXTRACT(YEAR FROM date_played), EXTRACT(MONTH FROM date_played)
-      round_per_month_db = [4, 12, 13, 14, 17, 16, 22, 16, 11, 6, 5, 7] #ActiveRecord call, obtain rounds from last 12 mos
-      @all_months = %w(January February March April May June July 
-        August September October November December)
-      @rounds_per_month = []
-      @db_rounds_per_month_year = db_rounds_per_month_year
-      @db_rounds_per_month_year.each do |r|
-        current = {}
-        current[:year] = r.year_num
-        current[:month_num] = r.month_num
-        
-        current[:month] = @all_months[r.month_num.to_i - 1]
-        current[:num_rounds] = r.num_rounds
-        @rounds_per_month << current
-      end
-      # 12.times { |m|
-      #   current = {}
-      #   current[:year] = m.to_i.months.ago.year
-      #   current[:month_num] = m.to_i.months.ago.month
-      #   m_index = m.to_i.months.ago.month - 1
-      #   current[:month] = @all_months[m_index]
-      #   current[:num_rounds] = round_per_month_db[m_index]
-      #   @rounds_per_month << current
-      # }
-      
-      @rounds_per_month
-    end
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -2,9 +2,41 @@ class Round < ActiveRecord::Base
   
   has_one :course
   has_one :user
+  has_many :foursomes
   
   def last_ten_rounds
     Round.find(:all, :limit => 10, :order => "DESC")
+  end
+  
+  def self.db_rounds_per_month_year
+    # num_rounds month_num year_num
+    rounds_by_month_year = Round.find(:all, 
+      :select => "Count(*) as num_rounds, EXTRACT(MONTH FROM date_played) as month_num,  EXTRACT(YEAR FROM date_played) as year_num", 
+      :group => "EXTRACT(YEAR FROM date_played), EXTRACT(MONTH FROM date_played)",
+      :order => "year_num DESC, month_num DESC",
+      :limit => 12)
+    
+    rounds_by_month_year
+  end
+  
+  def self.rounds_per_month_desc
+    # select Count(*), EXTRACT(MONTH FROM date_played),  EXTRACT(YEAR FROM date_played) 
+    # from rounds GROUP BY EXTRACT(YEAR FROM date_played), EXTRACT(MONTH FROM date_played)
+    @all_months = %w(January February March April May June July 
+      August September October November December)
+    @rounds_per_month = []
+    @db_rounds_per_month_year = db_rounds_per_month_year
+    @db_rounds_per_month_year.each do |r|
+      current = {}
+      current[:year] = r.year_num
+      current[:month_num] = r.month_num
+      
+      current[:month] = @all_months[r.month_num.to_i - 1]
+      current[:num_rounds] = r.num_rounds
+      @rounds_per_month << current
+    end
+    
+    @rounds_per_month
   end
   
 end
